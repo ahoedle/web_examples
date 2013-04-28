1b4ed86be9fe1cb04de005f35cee207aae26fc4e
Petra Nachbaur <pna@petras-macbook-air.fritz.box>
2013-04-28 18:24:17 +0200

    Nachrichten

---
+++ SQL/Nachrichten/db_verbindung.php
@@ -0,0 +1,12 @@
+<?php
+	$mysqli = new mysqli("localhost", "root", "root", "aktuell");
+		
+	if ($mysqli->connect_error) {
+		echo("Fehler bei der Verbindung: " . mysqli_connect_error);
+		exit;
+	}
+	
+	if (!$mysqli->set_charset("utf8")) {
+		echo("Fehler beim Laden von UTF8 " . $mysqli->error);
+	}
+?>
