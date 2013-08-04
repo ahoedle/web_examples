f3b9dc4264e5253aee060b581305a7396bb077f7
Petra Nachbaur <pna@Petras-MacBook-Air.local>
2013-08-04 18:40:45 +0200

    aef

--- Yii/trackstar/protected/config/main.php
+++ Yii/trackstar/protected/config/main.php
@@ -36,7 +36,6 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 		// uncomment the following to enable URLs in path-format
-		/*
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
@@ -45,7 +44,7 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
+		
 /*
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
@@ -72,11 +71,10 @@ return array(
 					'levels'=>'error, warning',
 				),
 				// uncomment the following to show log messages on web pages
-				/*
 				array(
 					'class'=>'CWebLogRoute',
 				),
-				*/
+				
 			),
 		),
 	),
